4118680020327a7c8b404ec2345e9fbb3a782540
Eatmeta <metagroul@gmail.com>
2022-10-27 17:35:41 +0300

    Implement a class LevenshteinCalculator that making pairs of tokenized text documents and calculating a Levenshtein distance between them

--- LevenshteinCalculator.cs
+++ LevenshteinCalculator.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Configuration;
 using System.Collections.Generic;
+using System.Linq;
 
 // Каждый документ — это список токенов. То есть List<string>.
 // Вместо этого будем использовать псевдоним DocumentTokens.
@@ -14,11 +13,38 @@ namespace Antiplagiarism
     {
         public List<ComparisonResult> CompareDocumentsPairwise(List<DocumentTokens> documents)
         {
-            return new List<ComparisonResult> {
-                new ComparisonResult(
-                    documents[0], 
-                    documents[1], 
-                    TokenDistanceCalculator.GetTokenDistance(documents[0][0], documents[1][0]))};
+            var result = new List<ComparisonResult>();
+            for (var i = 0; i < documents.Count; i++)
+            {
+                for (int j = i; j < documents.Count; j++)
+                {
+                    if (j != i)
+                        result.Add(new ComparisonResult(documents[i], documents[j],
+                            LevenshteinDistance(documents[i], documents[j])));
+                }
+            }
+            return result;
+        }
+
+        private static double LevenshteinDistance(DocumentTokens first, DocumentTokens second)
+        {
+            var opt = new double[first.Count + 1, second.Count + 1];
+            for (var i = 0; i <= first.Count; ++i) opt[i, 0] = i;
+            for (var i = 0; i <= second.Count; ++i) opt[0, i] = i;
+
+            for (var i = 1; i <= first.Count; ++i)
+            for (var j = 1; j <= second.Count; ++j)
+            {
+                var tokenDistance = TokenDistanceCalculator.GetTokenDistance(first[i - 1], second[j - 1]);
+                if (tokenDistance == 0)
+                    opt[i, j] = opt[i - 1, j - 1];
+                else
+                    opt[i, j] = new[]
+                    {
+                        1 + opt[i - 1, j], tokenDistance + opt[i - 1, j - 1], 1 + opt[i, j - 1]
+                    }.Min();
+            }
+            return opt[first.Count, second.Count];
         }
     }
 }
